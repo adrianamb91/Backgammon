c286c0d1aa05c3d32d00093d74bea67fbd2532de
Adriana Bogza <adriana.bogza@gmail.com>
2012-07-15 18:27:52 +0300

    Arrange functions; add function to compute destinations for player's piece and generate random numbers for dices

--- bot.py
+++ bot.py
@@ -1,44 +1,128 @@
 import board
-import random
 
-b = board.Board()
-for i in range(25):
-    print i, " ", b.allSpaces[i]
+
+class Bot :
+    
+    board = board.Board()
+    level = 0
+    
+    def __init__(self, level):
+        self.level = level
+        
+    
+    def check_bar(self) :
+        bar = self.board.allSpaces[0]
+        if (bar[0] != 0) :
+            return True
+        return False 
+    
+    def go_towards_house(self, dice) :
+        i = 1
+        t = False
+        while (t == False and i < 24) :
+            if (self.board.allSpaces[i][1] != self.board.computer) :
+                i += 1
+            else :
+                if (self.board.allSpaces[i+dice][1] == self.board.computer or 
+                    self.board.allSpaces[i+dice][1] == self.board.empty or 
+                    (self.board.allSpaces[i+dice][1] == self.board.player and 
+                     self.board.allSpaces[i+dice][0] == 1)) :
+                    t = True
+                else :
+                    i += 1
+        if (t == True) :
+            self.board.make_move(i, dice)
+        return
+    
+    def make_gate(self, dice1, dice2) :
+        i = 1
+        t = False
+        dif = abs(dice1-dice2)
+        while (t == False and i < 24) :
+            if (self.board.allSpaces[i][1] == self.board.computer and 
+                self.board.allSpaces[i+dif][1] == self.board.computer) :
+                #posibila poarta
+                if (self.board.allSpaces[i + max(dice1, dice2)][1] == self.board.empty or 
+                    (self.board.allSpaces[i + max(dice1, dice2)][1] == self.board.player and 
+                     self.board.allSpaces[i + max(dice1, dice2)][0] == 1 )) :
+                    t = True
+                else :
+                    i += 1  
+            else:
+                i += 1
+                
+        if (t == True) :
+            self.board.make_move(i, max(dice1, dice2))
+            self.board.make_move(i + dif, min(dice1, dice2))
+            print "Made gaaate!"
+            return True
+        else :
+            return False
+        
+        
+    def compute_move(self, dices) :
+        if (dices[0] == dices[1]) :
+            dice_usage = [False, False, False, False]
+        else :
+            dice_usage = [False, False]
+        
+        while (dice_usage.count(False) != 0) :
+            
+            if (self.check_bar() == False) :
+                # compute move
+                print "You can compute your move normally"
+                if (dices[0] != dices[1] and dice_usage[0] == False and dice_usage[1] == False and 
+                    self.make_gate(dices[0], dices[1]) == True) :
+                        print "Made gaaate!"
+                        dice_usage[1] = True
+                        dice_usage[0] = True
+                else:
+                    dice_index = dice_usage.index(False)
+                    self.go_towards_house(dices[dice_index])
+                    dice_usage[dice_index] = True
+            else :
+                # try to move piece from bar
+                print "You have to move your piece(s) from the bar first"           
+        return
+    
+    
+        
+bot = Bot(1)    
+
+
+# bot.board.printb()
     
 movement = 0
 
 while (movement != '-') :
     #player move
 
-    dice1 = random.randint(1, 6)
-    dice2 = random.randint(1, 6)
+    dices = bot.board.generate_dices()
     
-    for i in range(25):
-        print i, " ", b.allSpaces[i]
-        
-    print "Dices = ", dice1, " ", dice2
+    bot.board.printb()
+    
+    print "Dices = ", dices[0], " ", dices[1]
+    
+    #Test : player destinations for 24:
+    
+    #destinations = bot.board.get_player_destinations(24)
+    #print "Destinations = ", destinations
+    
     movement = input("Player Move: ")
     
     for i in range (0, len(movement), 2) :
-        b.move_player(movement[i], movement[i + 1]) 
+        bot.board.move_player(movement[i], movement[i + 1]) 
         
         
     # computer move
     
-    dice1 = random.randint(1, 6)
-    dice2 = random.randint(1, 6)
+    dices = bot.board.generate_dices()
     
-    if (dice1 == dice2) :
-        dices = (dice1, dice1, dice1, dice1)
-    else :
-        dices = (dice1, dice2)
     
-    for i in range(25):
-        print i, " ", b.allSpaces[i]
         
-    print "Dices = ", dice1, " ", dice2
+    print "Dices = ", dices[0], " ", dices[1]
     
-    b.compute_move(dices)
+    bot.compute_move(dices)
     
      
 
