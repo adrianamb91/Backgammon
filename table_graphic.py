8dda5458501a8096f306c99be06bd16f0b6f63bf
Cosmin Stamate <cos.stamate@yahoo.com>
2012-07-16 12:31:12 +0300

    Optimize pieces drawing; fix long lines in table_graphic.py

--- table_graphic.py
+++ table_graphic.py
@@ -5,7 +5,7 @@ import primitives as pm
 import gradient as gr
 import piece_graphic as piece
 
-import backgammon_config as conf
+import backgammon_config as cf
 
 
 class Table(object):
@@ -81,19 +81,19 @@ class Table(object):
 
 
     def resize(self):
-        temp_width = self.width - conf.BORDER_THICKNESS * 2
-        temp_height = temp_width * conf.RELATIVE_HEIGHT / conf.RELATIVE_WIDTH
+        temp_width = self.width - cf.BORDER_THICKNESS * 2
+        temp_height = temp_width * cf.RELATIVE_HEIGHT / cf.RELATIVE_WIDTH
         if temp_height > self.height:
-            temp_height = self.height - conf.BORDER_THICKNESS * 2
-            temp_width = temp_height * conf.RELATIVE_WIDTH / conf.RELATIVE_HEIGHT
+            temp_height = self.height - cf.BORDER_THICKNESS * 2
+            temp_width = temp_height * cf.RELATIVE_WIDTH / cf.RELATIVE_HEIGHT
 
         self.table_width = temp_width
         self.table_height = temp_height
 
 
     def draw_pieces(self, init = False):
-        width = self.temp_width['triangle'] * conf.PIECE_SIZE_PERCENTAGE
-        shadow_thickness = width * conf.PIECE_SHADOW_THICKNESS
+        width = self.temp_width['triangle'] * cf.PIECE_SIZE_PERCENTAGE
+        shadow_thickness = width * cf.PIECE_SHADOW_THICKNESS
         actual_width = width - shadow_thickness + 1
 
         if init:
@@ -113,52 +113,62 @@ class Table(object):
                 if 0 <= pos <= 5 or 18 <= pos <= 23:
                     half = 0
                 else: half = 1
-                factor_w = pos % 6
+                factor1 = pos % 6
+                factor2 = int(pos / 12)
+                if factor2 == 0: factor3 = 1
+                else: factor3 = -1
+                
+                offset_x_pc = (self.offset_x['half'][half] +
+                                self.temp_width['half'] * factor2 +
+                                    (self.temp_width['triangle'] / 2 +
+                                    self.temp_width['triangle'] * factor1) *
+                                    factor3)
 
-                if pos < 12:
-                    offset_x_pc = self.offset_x['half'][half] + self.temp_width['triangle'] / 2 + self.temp_width['triangle'] * factor_w
-                    offset_y_pc = self.offset_y['half'][half] + actual_width / 2 + actual_width * j
-                else:
-                    offset_x_pc = self.offset_x['half'][half] + self.temp_width['half'] - self.temp_width['triangle'] / 2 - self.temp_width['triangle'] * factor_w
-                    offset_y_pc = self.offset_y['half'][half] + self.temp_height['half'] - actual_width / 2 - actual_width * j
+                offset_y_pc = (self.offset_y['half'][half] +
+                                self.temp_height['half'] * factor2 +
+                                (actual_width / 2 + actual_width * j) * factor3)
 
                 if init:
                     self.pieces[i].append(
-                        piece.Piece(
-                            offset_x_pc, offset_y_pc, width, self.player_colors[spaces[i][1]],
-                            j == piece_nr - 1 and self.player_colors[spaces[i][1]] == self.PLAYER))
+                        piece.Piece(offset_x_pc, offset_y_pc, width,
+                            self.player_colors[spaces[i][1]],
+                            (j == piece_nr - 1 and
+                            self.player_colors[spaces[i][1]] == self.PLAYER)))
                 else:
                     self.pieces[i][j].draw(offset_x_pc, offset_y_pc, width,
-                            j == piece_nr - 1 and self.pieces[i][j].color == self.PLAYER)
+                            (j == piece_nr - 1 and
+                            self.pieces[i][j].color == self.PLAYER))
 
             if init: self.pieces.append([])
 
 
     def draw_canvas(self, init = False):
         if init:
-            self.background = pm.Rect(0, 0, self.width, self.height, conf.BG_COLOR)
+            self.background = pm.Rect(0, 0, self.width, self.height, cf.BG_COLOR)
         else:
-            self.background.draw(0, 0, self.width, self.height, conf.BG_COLOR)
+            self.background.draw(0, 0, self.width, self.height, cf.BG_COLOR)
 
 
     def draw_table_border(self, init = False):
         self.offset_x['global'] = (self.width - self.table_width) / 2
         self.offset_y['global'] = (self.height - self.table_height) / 2
-        self.inner_border_thickness = self.table_width * conf.INNER_BORDER_THICKNESS
-        gradient_end = self.inner_border_thickness * conf.TABLE_BORDER_SHADOW_THICKNESS
+        self.inner_border_thickness = (self.table_width *
+                                            cf.INNER_BORDER_THICKNESS)
+        gradient_end = (self.inner_border_thickness *
+                                            cf.TABLE_BORDER_SHADOW_THICKNESS)
 
         if init:
             self.table_border = pm.Rect(self.offset_x['global'],
                                         self.offset_y['global'],
                                         self.table_width,
                                         self.table_height,
-                                        conf.TABLE_BORDER_COLOR)
+                                        cf.TABLE_BORDER_COLOR)
 
             self.table_border_shadow = gr.RectGradient(
                                 self.offset_x['global'], self.offset_y['global'],
                                 self.table_width, self.table_height,
-                                conf.TABLE_BORDER_SHADOW_START_COLOR,
-                                conf.TABLE_BORDER_SHADOW_END_COLOR,
+                                cf.TABLE_BORDER_SHADOW_START_COLOR,
+                                cf.TABLE_BORDER_SHADOW_END_COLOR,
                                 0, gradient_end)
 
         else:
@@ -166,7 +176,7 @@ class Table(object):
                                     self.offset_y['global'],
                                     self.table_width,
                                     self.table_height,
-                                    conf.TABLE_BORDER_COLOR)
+                                    cf.TABLE_BORDER_COLOR)
 
             self.table_border_shadow.draw(
                                 self.offset_x['global'], self.offset_y['global'],
@@ -175,38 +185,47 @@ class Table(object):
 
 
     def draw_table_3d(self, init = False):
-        self.temp_width['illusion'] = self.table_width / 2 - self.inner_border_thickness * 2
-        self.temp_height['illusion'] = self.table_height - self.inner_border_thickness * 2
-        self.inner_3d_thickness = self.table_width * conf.TABLE_INNER_3D_THICKNESS
+        self.temp_width['illusion'] = (self.table_width / 2 -
+                                                self.inner_border_thickness * 2)
+        self.temp_height['illusion'] = (self.table_height -
+                                                self.inner_border_thickness * 2)
+        self.inner_3d_thickness = self.table_width * cf.TABLE_INNER_3D_THICKNESS
 
         if init:
             self.halves_3d = []
 
         for i in range(2):
-            offset_x_il = self.offset_x['global'] + self.temp_width['illusion'] * i + self.inner_border_thickness * (2 * i + 1)
+            offset_x_il = (self.offset_x['global'] + self.temp_width['illusion']
+                            * i + self.inner_border_thickness * (2 * i + 1))
             offset_y_il = self.offset_y['global'] + self.inner_border_thickness
 
             if init:
                 self.halves_3d.append(gr.RectGradient(
                                 offset_x_il, offset_y_il,
-                                self.temp_width['illusion'], self.temp_height['illusion'],
-                                conf.TABLE_INNER_3D_SHADOW_START_COLOR,
-                                conf.TABLE_INNER_3D_SHADOW_END_COLOR,
+                                self.temp_width['illusion'],
+                                self.temp_height['illusion'],
+                                cf.TABLE_INNER_3D_SHADOW_START_COLOR,
+                                cf.TABLE_INNER_3D_SHADOW_END_COLOR,
                                 0, self.inner_3d_thickness))
             else:
                 self.halves_3d[i].draw(offset_x_il, offset_y_il,
-                                self.temp_width['illusion'], self.temp_height['illusion'],
+                                self.temp_width['illusion'],
+                                self.temp_height['illusion'],
                                 0, self.inner_3d_thickness)
 
 
     def draw_table_halves(self, init = False):
         thickness = self.inner_border_thickness + self.inner_3d_thickness
-        self.temp_width['half'] = self.temp_width['illusion'] - self.inner_3d_thickness * 2
-        self.temp_height['half'] = self.temp_height['illusion'] - self.inner_3d_thickness * 2
+        self.temp_width['half'] = (self.temp_width['illusion'] -
+                                                self.inner_3d_thickness * 2)
+        self.temp_height['half'] = (self.temp_height['illusion'] -
+                                                self.inner_3d_thickness * 2)
         if self.temp_width['half'] < self.temp_height['half']:
-            gradient_end = self.temp_width['half'] * conf.TABLE_HALF_BG_SHADOW_THICKNESS
+            gradient_end = (self.temp_width['half'] *
+                                            cf.TABLE_HALF_BG_SHADOW_THICKNESS)
         else:
-            gradient_end = self.temp_height['half'] * conf.TABLE_HALF_BG_SHADOW_THICKNESS
+            gradient_end = (self.temp_height['half'] *
+                                            cf.TABLE_HALF_BG_SHADOW_THICKNESS)
 
         if init:
             self.halves_bg = []
@@ -215,7 +234,9 @@ class Table(object):
         self.offset_x['half'] = []
         self.offset_y['half'] = []
         for i in range(2):
-            self.offset_x['half'].append(self.offset_x['global'] + self.temp_width['half'] * i + thickness * (2 * i + 1))
+            self.offset_x['half'].append(self.offset_x['global'] +
+                                            self.temp_width['half'] * i +
+                                            thickness * (2 * i + 1))
             self.offset_y['half'].append(self.offset_y['global'] + thickness)
 
             if init:
@@ -223,33 +244,38 @@ class Table(object):
                                                 self.offset_y['half'][i],
                                                 self.temp_width['half'],
                                                 self.temp_height['half'],
-                                                conf.TABLE_HALF_BG_COLOR))
+                                                cf.TABLE_HALF_BG_COLOR))
 
                 self.halves_bg_shadow.append(gr.RectGradient(
-                                self.offset_x['half'][i], self.offset_y['half'][i],
-                                self.temp_width['half'], self.temp_height['half'],
-                                conf.TABLE_HALF_BG_SHADOW_START_COLOR,
-                                conf.TABLE_HALF_BG_SHADOW_END_COLOR,
+                                self.offset_x['half'][i],
+                                self.offset_y['half'][i],
+                                self.temp_width['half'],
+                                self.temp_height['half'],
+                                cf.TABLE_HALF_BG_SHADOW_START_COLOR,
+                                cf.TABLE_HALF_BG_SHADOW_END_COLOR,
                                 0, gradient_end))
             else:
                 self.halves_bg[i].draw(self.offset_x['half'][i],
                                         self.offset_y['half'][i],
                                         self.temp_width['half'],
                                         self.temp_height['half'],
-                                        conf.TABLE_HALF_BG_COLOR)
+                                        cf.TABLE_HALF_BG_COLOR)
 
                 self.halves_bg_shadow[i].draw(
-                                self.offset_x['half'][i], self.offset_y['half'][i],
-                                self.temp_width['half'], self.temp_height['half'],
+                                self.offset_x['half'][i],
+                                self.offset_y['half'][i],
+                                self.temp_width['half'],
+                                self.temp_height['half'],
                                 0, gradient_end)
 
 
     def draw_table_triangles(self, init = False):
         draw_color = 0
-        colors = [conf.TABLE_TRIANGLE_BLACK_COLOR, conf.TABLE_TRIANGLE_RED_COLOR]
+        colors = [cf.TABLE_TRIANGLE_BLACK_COLOR, cf.TABLE_TRIANGLE_RED_COLOR]
         self.temp_width['triangle'] = self.temp_width['half'] / 6
-        triangle_spacing = self.temp_width['triangle'] * conf.TRIANGLE_SPACING
-        self.temp_height['triangle'] = self.temp_height['half'] * conf.TRIANGLE_HEIGHT
+        triangle_spacing = self.temp_width['triangle'] * cf.TRIANGLE_SPACING
+        self.temp_height['triangle'] = (self.temp_height['half'] *
+                                                            cf.TRIANGLE_HEIGHT)
         triangle_pos = 1
 
         self.offset_x['tr_quarter'] = []
@@ -262,15 +288,25 @@ class Table(object):
 
             for j in range(2):
                 self.offset_x['tr_quarter'].append(self.offset_x['half'][j])
-                self.offset_y['tr_quarter'].append(self.offset_y['half'][j] + offset_y_trh)
+                self.offset_y['tr_quarter'].append(self.offset_y['half'][j] +
+                                                                offset_y_trh)
 
                 for k in range(6):
-                    vertex_array = [(self.offset_x['tr_quarter'][i*2 + j] + self.temp_width['triangle'] * k + triangle_spacing ,
+                    vertex_array = [(self.offset_x['tr_quarter'][i*2 + j] +
+                                        self.temp_width['triangle'] * k +
+                                        triangle_spacing ,
                                         self.offset_y['tr_quarter'][i*2 + j]) ,
-                                     (self.offset_x['tr_quarter'][i*2 + j] + self.temp_width['triangle'] * (k + 1) - triangle_spacing,
+
+                                     (self.offset_x['tr_quarter'][i*2 + j] +
+                                        self.temp_width['triangle'] * (k + 1) -
+                                        triangle_spacing,
                                         self.offset_y['tr_quarter'][i*2 + j]) ,
-                                     (self.offset_x['tr_quarter'][i*2 + j] + self.temp_width['triangle'] * (k + 0.5),
-                                        self.offset_y['tr_quarter'][i*2 + j] + self.temp_height['triangle'] * triangle_pos)]
+
+                                     (self.offset_x['tr_quarter'][i*2 + j] +
+                                        self.temp_width['triangle'] * (k + 0.5),
+                                        self.offset_y['tr_quarter'][i*2 + j] +
+                                        self.temp_height['triangle'] *
+                                        triangle_pos)]
 
                     if init:
                         self.triangles.append(pm.Polygon(vertex_array,
@@ -289,33 +325,36 @@ class Table(object):
 
 
     def draw_table_home_labels(self, init = False):
-        label_width = self.table_width * conf.HOME_LABEL_WIDTH
-        label_height = self.inner_border_thickness * conf.HOME_LABEL_HEIGHT
+        label_width = self.table_width * cf.HOME_LABEL_WIDTH
+        label_height = self.inner_border_thickness * cf.HOME_LABEL_HEIGHT
 
-        corner_radius = label_width * conf.HOME_LABEL_CORNER_RADIUS
-        lb_text = [conf.HOME_LABEL_TEXT_PLAYER, conf.HOME_LABEL_TEXT_COMPUTER]
+        corner_radius = label_width * cf.HOME_LABEL_CORNER_RADIUS
+        lb_text = [cf.HOME_LABEL_TEXT_PLAYER, cf.HOME_LABEL_TEXT_COMPUTER]
 
         if init:
             self.labels = []
 
         for i in range(2):
-            offset_x_lb = self.offset_x['global'] + self.table_width * conf.HOME_LABEL_SPACER
-            offset_y_lb = self.offset_y['global'] + self.table_height * i - self.inner_border_thickness * i + (self.inner_border_thickness - label_height) / 2
+            offset_x_lb = (self.offset_x['global'] + self.table_width *
+                                cf.HOME_LABEL_SPACER)
+            offset_y_lb = (self.offset_y['global'] + self.table_height * i -
+                            self.inner_border_thickness * i +
+                            (self.inner_border_thickness - label_height) / 2)
 
             if init:
                 self.labels.append(pm.RoundedLabel(offset_x_lb, offset_y_lb,
                                    label_width, label_height, corner_radius,
-                                   conf.HOME_LABEL_BG_COLOR, lb_text[i],
-                                   conf.HOME_LABEL_TEXT_PROPORTION,
-                                   conf.HOME_LABEL_TEXT_FONT,
-                                   conf.HOME_LABEL_FG_COLOR))
+                                   cf.HOME_LABEL_BG_COLOR, lb_text[i],
+                                   cf.HOME_LABEL_TEXT_PROPORTION,
+                                   cf.HOME_LABEL_TEXT_FONT,
+                                   cf.HOME_LABEL_FG_COLOR))
             else:
                 self.labels[i].draw(offset_x_lb, offset_y_lb,
                                    label_width, label_height, corner_radius,
-                                   conf.HOME_LABEL_BG_COLOR, lb_text[i],
-                                   conf.HOME_LABEL_TEXT_PROPORTION,
-                                   conf.HOME_LABEL_TEXT_FONT,
-                                   conf.HOME_LABEL_FG_COLOR)
+                                   cf.HOME_LABEL_BG_COLOR, lb_text[i],
+                                   cf.HOME_LABEL_TEXT_PROPORTION,
+                                   cf.HOME_LABEL_TEXT_FONT,
+                                   cf.HOME_LABEL_FG_COLOR)
 
 if __name__ == '__main__':
     "Please do not run this file directly, include it."
